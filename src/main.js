eb75588982240264ab81567ff04bd65bf38cf641
Wesley Guirra <wesleyguirra@fabmvp.com.br>
2018-04-16 22:46:11 -0300

    Remove idea files and start validating fields

--- src/main.js
+++ src/main.js
@@ -1,86 +1,141 @@
-function validateForm () {
+const form = document.getElementById("formulario_teste");
 
-    // Inicia as váriaveis
-    const name = document.getElementById("name").value;
-    const cpf = document.getElementById("cpf").value;
-    const email = document.getElementById("email").value;
-    const obs = document.getElementById("obs").value;
-
-
-    // Checa função de validação do CPF
-    if (!validateCPF(cpf)) {
-        document.getElementById("msg_cpf")
-            .innerHTML = "Campo Obrigatório: Digite um CPF no formato XXX.XXX.XXX-XX.";
-        console.log(cpf);
-
-    } else {
-        document.getElementById("msg_cpf")
-            .innerHTML = "";
-
-    }
-
-    // Checa função de validação do Nome
-    if (!validateFullName(name)) {
-        document.getElementById("msg_name")
-            .innerHTML = "Campo Obrigatório: Digite seu nome completo!";
-
-    }
+/**
+ * Adicionado EventListener para capturar o envio do form com enter
+ */
+if (form.addEventListener) {
+    form.addEventListener("submit", validateForm(form));
+} else if (form.attachEvent) {
+    /*
+     * O addEventListener não é suportado em versões mais antigas do IE,
+     * por isso estou verificando o attachEvent também.
+     * https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Browser_compatibility
+     */
+    form.attachEvent("onsubmit", validateForm(form));
+}
 
 
-    // Checa função de validação do Email
-    if (!validateEmail(email)) {
-        document.getElementById("msg_email")
-            .innerHTML = "Campo Obrigatório: Digite um email válido!";
+/**
+ *
+ * @param form Recebe o formulário que será validado
+ */
+function validateForm (form) {
+    /**
+     *
+     * @type {*[]} Array com os objetos dos campos do formulário
+     */
+    const form_fields = [].slice.call(form.querySelectorAll('select, input:not([type=radio]):not([type=checkbox])') || []);
 
-    }
+    validateEmptyFields(form_fields);
 
-    // Checa função de validação Observações
-    if (validateObs(obs)) {
-        document.getElementById("msg_obs")
-            .innerHTML = "Campo Obrigatório: Máximo de 600 caracteres!";
-    }
+    validateFieldsFormat(form_fields);
+}
 
-
-    // Funções
-    function validateObs(obs) {
-        return (obs.length <= 600) && (obs !== "" || obs !== null);
-    }
-
-    function validateFullName(name) {
-        const RegExp =/^((?![ .]+$)[a-záàâãéèêíïóôõöúçñ A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ .]{10,50})$/;
-
-        if (name === null || name === "") {
-            return false;
+function validateFieldsFormat(fields) {
+    for (field of fields) {
+        switch (field.name) {
+            case "cpf":
+                (!isCpfFormat(field.value))
+                    ? field.offsetParent.lastElementChild.innerHTML = "Formato de CPF Inválido"
+                    : console.log("campo CPF Validado");
+                break;
+            case "name":
+                validateFullName(field.value);
+                break;
+            case "obs":
+                validateObs(field.value);
+                break;
+            case "email":
+                validateEmail(field.value);
+                break;
+            default:
+                return;
         }
+    } 
+}
 
-        return RegExp.test(name);
-    }
 
-    function validateEmail(email) {
-        const RegExp = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/;
+/**
+ *
+ * @param obs valor que será validado
+ * @returns {boolean} Retorna se o número de caracteres do campo é menor ou igual à 600
+ * e não nulo ou vazio
+ */
+function isLengthSixthy(obs) {
+    return (obs.length <= 600) && (obs !== "" || obs != null);
+}
 
-        if (name === null || name === "") {
-            return false;
+/**
+ *
+ * @param name O valor do nome que será validado
+ * @returns {boolean} Retorna o test do RegEx do nome deve ser um nome completo
+ * entre 10 e 50 caracteres com espaço entre primeiro e segundo nome.
+ */
+function isFullName(name) {
+    const RegExp =/^((?![ .]+$)[a-záàâãéèêíïóôõöúçñ A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ .]{10,50})$/;
+
+    return RegExp.test(name);
+}
+
+/**
+ *
+ * @param email O valor do email que será validado
+ * @returns {boolean} Retorna o test do RegEx do email
+ */
+function isValidEmail(email) {
+    /* Verificando se o texto recebido é iniciado por caracteres
+     * alfanumericos e alguns especiais (usuário) (^[A-Z0-9._%+-]);
+     * seguido de (@)
+     * seguido de caracteres alfanumericos pontos ou hifens (domínio) ([A-Z0-9.-]);
+     * seguido de ponto (\.);
+     * seguido (e finalizado por no mínimo duas letras (extensão do dominio).
+     */
+    const RegExp = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/;
+
+    return RegExp.test(email);
+}
+
+/**
+ *
+ * @param cpf O valor do cpf que será validado
+ * @returns {boolean} Retorna o test do RegEx do cpf que deve começar
+ * com 3 digitos seguido de 1 ponto, seguido de 3 digitos, seguido de 1 ponto,
+ * seguido de 3 digitos, seguido de hifen, seguido de 2 digitos
+ * Ex: 123.456.789-10
+ */
+function isCpfFormat(cpf) {
+    /* Verificando se o texto recebido é iniciado por três números (^[\d]{3});
+     * seguidos de ponto (\.);
+     * seguido de três números ([\d]{3});
+     * seguidos de ponto (\.);
+     * seguido de três números ([\d]{3});
+     * seguidos de hífen (\-);
+     * seguido (e finalizado) por dois números ([\d]{2}$);
+    */
+    const RegExp = /^[\d]{3}\.[\d]{3}\.[\d]{3}\-[\d]{2}$/;
+
+    return RegExp.test(cpf);
+}
+
+/**
+ *
+ * @param fields Os campos que serão checados
+ */
+function validateEmptyFields(fields) {
+    for (field of fields) {
+        if (isEmptyValue(field.value)) {
+            field.offsetParent.lastElementChild.innerHTML = "Campo Obrigatório!";
         }
-
-        return RegExp.test(email);
     }
+}
 
-    function validateCPF(cpf) {
-        /* Verificando se o texto recebido é iniciado por três números (^[\d]{3});
-        seguidos de ponto (\.);
-        seguido de três números ([\d]{3});
-        seguidos de ponto (\.);
-        seguido de três números ([\d]{3});
-        seguidos de hífen (\-);
-        seguido (e finalizado) por dois números ([\d]{2}$);
-        */
-        const RegExp = /^[\d]{3}\.[\d]{3}\.[\d]{3}\-[\d]{2}$/;
-
-        if (name === null || name === "") {
-            return false;
-        }
-        return RegExp.test(cpf);
+/**
+ *
+ * @param value Valor para comparação com uma string vazia ou null
+ * @returns {boolean} retorna true se a string está vazia
+ */
+function isEmptyValue(value) {
+    if (value === "" || value == null) {
+        return true;
     }
-
 }
